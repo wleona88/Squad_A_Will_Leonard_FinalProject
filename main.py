2fe21b9687a40cd2c8b5e47160591f0f275e5568
wleona88 <64604120+wleona88@users.noreply.github.com>
2020-06-16 10:47:13 -0400

    Teams endpoint now excludes unecessary data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 
 #froms
-from  flask import Flask, jsonify, request, Response
+from  flask import Flask, jsonify, request, render_template, Response
 
 app = Flask(__name__)
 
@@ -13,13 +13,24 @@ app = Flask(__name__)
 def home():
 	Teams_URL = "https://statsapi.web.nhl.com/api/v1/teams"
 	r = requests.get(Teams_URL)
-	return r.json()
+	r = r.json()
+	return render_template("home.html")
 
 @app.route('/team/<ID>')
 def teams(ID):
 	One_Team_URL = ("https://statsapi.web.nhl.com/api/v1/teams/"+ID+"?expand=team.stats")
 	r = requests.get(One_Team_URL)
-	return r.json()
+	r = r.json()
+	team_data = r["teams"][0]
+	team_stats = team_data["teamStats"][0]
+	team_splits = team_stats["splits"][0]
+	final_stats = team_splits["stat"]
+	team_api_info = team_splits["team"]
+	final_team_name = team_api_info["name"]
+	final_stats["team"] = final_team_name
+	
+	return f"The stats of the {final_team_name} are: {final_stats}"
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
