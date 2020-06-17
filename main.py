63ce48b0e085f20827c9381242a5c0359eb1dda4
wleona88 <64604120+wleona88@users.noreply.github.com>
2020-06-16 20:43:57 -0400

    Added html functionality on teams page

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from  flask import Flask, jsonify, request, render_template, Response
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/.html')
 def home():
 	Teams_URL = "https://statsapi.web.nhl.com/api/v1/teams"
 	r = requests.get(Teams_URL)
 	r = r.json()
 	return render_template("home.html")
 
-@app.route('/team/<ID>')
+@app.route('/teamID/<ID>.html')
 def teams(ID):
 	One_Team_URL = ("https://statsapi.web.nhl.com/api/v1/teams/"+ID+"?expand=team.stats")
 	r = requests.get(One_Team_URL)
@@ -27,10 +27,11 @@ def teams(ID):
 	final_stats = team_splits["stat"]
 	team_api_info = team_splits["team"]
 	final_team_name = team_api_info["name"]
+	team = final_team_name
 	final_stats["team"] = final_team_name
 	
-	return f"The stats of the {final_team_name} are: {final_stats}"
-
+	#return f"The stats of the {final_team_name} are: {final_stats}"
+	return render_template("team.html", team=team)
 
 if __name__ == "__main__":
 	app.run(debug=True)
